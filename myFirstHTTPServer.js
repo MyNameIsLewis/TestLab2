c112cf7539d679f465886c79ef0b198c10666b78
MyNameIsLewis <lewiskearsey@gmail.com>
2015-10-19 13:09:18 +0100

    Update myFirstHTTPServer.js

--- myFirstHTTPServer.js
+++ myFirstHTTPServer.js
@@ -22,12 +22,13 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 //Why do we this? 
 //What does it do?
 
-var nomo = require('node-monkey').start({host:server_ip_address, port:'8000'} );
+// Use a remote logging console
+var consolere = require('console-remote-client').connect('console.re','80','mynameislewis_first_server');
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
     response.end('Yes! It Works!! Path Hit: ' + server_ip_address + ":" + server_port + "/" + request.url);
-    console.log("my server ip address is: " + server_ip_address);
+    console.re.log("My server ip address is: " + server_ip_address);
 }
 //What is a request?
 //What is a response?
@@ -46,7 +47,7 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(server_port, server_ip_address, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("HTTP Server listening on: " + server_ip_address + ":" + server_port);
+    console.re.log("HTTP Server listening on: " + server_ip_address + ":" + server_port);
 });
 //What does the listen function do?
 //What is console.log?
